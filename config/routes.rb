dbdb921780349d7065279f45dcb9240d2f38739a
kohey7788 <kohey.chelsea1017@gmail.com>
2020-12-15 13:57:09 +0000

    Initial Commit

---
+++ config/routes.rb
@@ -0,0 +1,6 @@
+Rails.application.routes.draw do
+  # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.htm
+  resources :books
+  root :to => 'index#top'
+  get 'books/:id' => 'books#show'
+end
